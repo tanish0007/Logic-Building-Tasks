62a469f8b818534995f85f6d2f6b7d35b9f442e5
tanish0007 <jangratanish0007@gmail.com>
2025-07-09 01:03:32 +0530

    reverse word  order is also done

--- task-37.cpp
+++ task-37.cpp
@@ -3,22 +3,57 @@
 #include <string>
 using namespace std;
 
-void revWordsOrder(string &str) {
-    string newString = "";
-    for(int i = 0; i<str.length(); i++){
-        if(!(str[i] == ' ' || str[i] == '\n' || str[i] == '\t')){
-            
-        }
+void reverse(string &str, int start, int end){
+    while(start < end){
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        
+        start++;
+        end--;
     }
+}
+
+void revWordsString(string &str){
+    int j=0, k=0;
+    for(int i=0; i<str.length(); ){
+        if(!(str[i] == ' ' || str[i] == '\n' || str[i] == '\t')){
+            k=i;
+        }
+        else{
+            reverse(str, j, k);
+            while(str[i] == ' ' || str[i] == '\n' || str[i] == '\t'){
+                i++;
+            }
+            j = k = i;
+        }
+        if(i == str.length()-1){
+            reverse(str, j, k);
+            j = k = i+1;
+        }
+        i++;
+    }
+    cout << str;
+}
+
+string reverse(string &str){
+    string rev = "";
+    for(int i=str.length()-1; i>=0; i--){
+        rev += str[i];
+    }
+    return rev;
+}
+
+void revWordsOrder(string &str) {
+    string newString = reverse(str);
+    revWordsString(newString);
 }
 
 int main() {
     string str;
     cout << "Enter sentence: ";
     getline(cin, str);
-
     revWordsOrder(str);
-    cout << str;
 
     return 0;
 }
